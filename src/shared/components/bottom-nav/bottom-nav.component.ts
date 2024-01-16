9133ee0393d96a21b8b20ed668cbdae49444aad7
gajevski <mikolajxgajewski@gmail.com>
2024-01-16 14:09:56 +0100

    add tab switch on route change

--- src/shared/components/bottom-nav/bottom-nav.component.ts
+++ src/shared/components/bottom-nav/bottom-nav.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { NavigationEnd, Router, RouterLink } from '@angular/router';
+import { filter } from 'rxjs';
 
 @Component({
   selector: 'app-bottom-nav',
@@ -8,6 +9,20 @@ import { RouterLink } from '@angular/router';
   templateUrl: './bottom-nav.component.html',
   styleUrl: './bottom-nav.component.scss'
 })
-export class BottomNavComponent {
+export class BottomNavComponent implements OnInit {
+  public currentRoute!: string;
+  
+  constructor(private _router: Router) {}
 
+  public ngOnInit(): void {
+    this._router.events.pipe(
+      filter((event): event is NavigationEnd => event instanceof NavigationEnd)
+    ).subscribe((event: NavigationEnd) => {
+      this.currentRoute = event.url;
+    });
+  }
+
+  public isActive(route: string): boolean {
+    return this.currentRoute === route;
+  }
 }
