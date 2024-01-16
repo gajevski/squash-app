69d0b0d611c2e2ce3cdbc5d2e95760077684e661
gajevski <mikolajxgajewski@gmail.com>
2024-01-16 13:38:48 +0100

    add routing for profile component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,16 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from '../views/home/home.component';
+import { ProfileComponent } from '../views/profile/profile.component';
 
 export const routes: Routes = [
     {
         path: '',
         title: 'SquashAPP - Home Page',
         component: HomeComponent
+    },
+    {
+        path: 'profile',
+        title: `SquashAPP - Player's Profile`,
+        component: ProfileComponent
     }
 ];
