20cac2cf850d6297495cee29e5f5c1706e8286ef
gajevski <mikolajxgajewski@gmail.com>
2024-01-16 13:40:28 +0100

    add routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from '../views/home/home.component';
 import { ProfileComponent } from '../views/profile/profile.component';
+import { HistoryComponent } from '../views/history/history.component';
 
 export const routes: Routes = [
     {
@@ -12,5 +13,10 @@ export const routes: Routes = [
         path: 'profile',
         title: `SquashAPP - Player's Profile`,
         component: ProfileComponent
+    },
+    {
+        path: 'history',
+        title: `SquashAPP - Player's History`,
+        component: HistoryComponent
     }
 ];
