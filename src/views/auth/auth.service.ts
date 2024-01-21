081107a7c9e558b262ff71a664bd49f990b94c8f
gajevski <mikolajxgajewski@gmail.com>
2024-01-21 16:56:43 +0100

    Create example API call in auth service

--- src/views/auth/auth.service.ts
+++ src/views/auth/auth.service.ts
@@ -1,13 +1,15 @@
-import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Injectable, inject } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
+  private _http: HttpClient = inject(HttpClient)
 
-  constructor() { }
-
-  public auth(): void {
+  public auth(): Observable<any> {
     console.log('auth called');
+    return this._http.get('http://localhost:3000/api/auth/callback/github/')
   }
 }
