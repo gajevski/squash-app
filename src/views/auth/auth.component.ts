e5973a214c6b9a6f9c221e5194178c20248fa98d
gajevski <mikolajxgajewski@gmail.com>
2024-01-21 16:59:35 +0100

    Use takeUntil to prevent memory leaks in subscription

--- src/views/auth/auth.component.ts
+++ src/views/auth/auth.component.ts
@@ -1,5 +1,6 @@
-import { Component, inject } from '@angular/core';
+import { Component, OnDestroy, inject } from '@angular/core';
 import { AuthService } from './auth.service';
+import { Subject, takeUntil } from 'rxjs';
 
 @Component({
   selector: 'app-auth',
@@ -8,10 +9,18 @@ import { AuthService } from './auth.service';
   templateUrl: './auth.component.html',
   styleUrl: './auth.component.scss'
 })
-export class AuthComponent {
+export class AuthComponent implements OnDestroy {
   private _authService: AuthService = inject(AuthService);
+  private _onDestroy$: Subject<boolean> = new Subject<boolean>
 
   public authUser(): void {
-    this._authService.auth().subscribe();
+    this._authService.auth().pipe(
+      takeUntil(this._onDestroy$)
+    ).subscribe();
+  }
+
+  public ngOnDestroy(): void {
+    this._onDestroy$.next(true);  
+    this._onDestroy$.unsubscribe();
   }
 }
