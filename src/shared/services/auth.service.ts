02f2483732fbc12c5780c733294756e2c1cdb144
gajevski <mikolajxgajewski@gmail.com>
2024-01-24 19:52:04 +0100

    Add redirect function in auth service

--- src/shared/services/auth.service.ts
+++ src/shared/services/auth.service.ts
@@ -1,9 +1,15 @@
-import { Injectable } from '@angular/core';
+import { Injectable, inject } from '@angular/core';
+import { environment } from '../../enviroment';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
+  private _http: HttpClient = inject(HttpClient);
+  private _baseUrl: string = environment.apiBaseUrl;
 
-  constructor() { }
+  public redirectToGitHub(): void {
+    window.location.href = `${this._baseUrl}/login`;
+  }
 }
