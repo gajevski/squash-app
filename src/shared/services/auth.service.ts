e852c6499fcd5ceb44b2e62054e84a920d82c3d1
gajevski <mikolajxgajewski@gmail.com>
2024-01-24 19:53:45 +0100

    Add exchangeCodeForToken function

--- src/shared/services/auth.service.ts
+++ src/shared/services/auth.service.ts
@@ -1,6 +1,7 @@
 import { Injectable, inject } from '@angular/core';
 import { environment } from '../../enviroment';
 import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -11,5 +12,9 @@ export class AuthService {
 
   public redirectToGitHub(): void {
     window.location.href = `${this._baseUrl}/login`;
+  }
+
+ public exchangeCodeForToken(code: string): Observable<any> {
+    return this._http.get<any>(`${this._baseUrl}/callback?code=${code}`);
   }
 }
