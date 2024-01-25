b734ba07975ea99fc06dc7bed2a6859a36fce29e
gajevski <mikolajxgajewski@gmail.com>
2024-01-25 12:27:41 +0100

    Update auth service with logout function

--- src/shared/services/auth.service.ts
+++ src/shared/services/auth.service.ts
@@ -2,12 +2,14 @@ import { Injectable, inject } from '@angular/core';
 import { environment } from '../../enviroment';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
   private _http: HttpClient = inject(HttpClient);
+  private _router: Router = inject(Router);
   private _baseUrl: string = environment.apiBaseUrl;
 
   public loginWithGitHub(): void {
@@ -16,5 +18,10 @@ export class AuthService {
 
  public exchangeCodeForToken(code: string): Observable<any> {
     return this._http.get<any>(`${this._baseUrl}/callback?code=${code}`);
+  }
+
+  public logout(): void {
+    localStorage.removeItem('token');
+    this._router.navigate(['/auth']);
   }
 }
