313666a10f592ef382c9c8eb437c1dddcfdca5fc
gajevski <mikolajxgajewski@gmail.com>
2024-01-27 18:15:22 +0100

    Use localstorage service in jwt interceptor

--- src/shared/interceptors/jwt.interceptor.ts
+++ src/shared/interceptors/jwt.interceptor.ts
@@ -1,11 +1,13 @@
-import { Injectable } from '@angular/core';
+import { Injectable, inject } from '@angular/core';
 import { HttpRequest, HttpHandler, HttpInterceptor } from '@angular/common/http';
+import { LocalstorageService } from '../services/localstorage.service';
 
 @Injectable()
 export class JwtInterceptor implements HttpInterceptor {
+  private _localStorage: LocalstorageService = inject(LocalstorageService); 
 
   intercept(request: HttpRequest<any>, next: HttpHandler) {
-    const token: string | null = localStorage.getItem('token');
+    const token: string | null = this._localStorage.getItem('token');
     if (token) {
       request = request.clone({
         setHeaders: {
