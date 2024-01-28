6791618967d343a0cd063e139a64e6e8a19f46e3
gajevski <mikolajxgajewski@gmail.com>
2024-01-28 14:33:59 +0100

    Use optimized image

--- src/views/profile/profile.component.ts
+++ src/views/profile/profile.component.ts
@@ -2,13 +2,13 @@ import { Component, inject } from '@angular/core';
 import { AuthService } from '../../shared/services/auth.service';
 import { UserService } from '../../shared/services/user.service';
 import { Observable } from 'rxjs';
-import { CommonModule } from '@angular/common';
+import { CommonModule, NgOptimizedImage } from '@angular/common';
 import { User } from '../../shared/models/user';
 
 @Component({
   selector: 'app-profile',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, NgOptimizedImage],
   templateUrl: './profile.component.html',
   styleUrl: './profile.component.scss'
 })
