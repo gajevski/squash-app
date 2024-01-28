747750373fffa5c574360d1a8403e3119adaaa0f
gajevski <mikolajxgajewski@gmail.com>
2024-01-28 14:28:37 +0100

    Fetch user data

--- src/views/profile/profile.component.ts
+++ src/views/profile/profile.component.ts
@@ -1,15 +1,21 @@
 import { Component, inject } from '@angular/core';
 import { AuthService } from '../../shared/services/auth.service';
+import { UserService } from '../../shared/services/user.service';
+import { Observable } from 'rxjs';
+import { CommonModule } from '@angular/common';
+import { User } from '../../shared/models/user';
 
 @Component({
   selector: 'app-profile',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './profile.component.html',
   styleUrl: './profile.component.scss'
 })
 export class ProfileComponent {
   private _authService: AuthService = inject(AuthService);
+  private _userService: UserService = inject(UserService);
+  public user$: Observable<User> = this._userService.getUser();
 
   public logout(): void {
     this._authService.logout();
