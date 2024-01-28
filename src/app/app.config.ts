bdf04e7d8368647aba285212eea7a51bd5406e10
gajevski <mikolajxgajewski@gmail.com>
2024-01-28 14:34:05 +0100

    Use optimized image

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -5,8 +5,10 @@ import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
 import { HTTP_INTERCEPTORS, provideHttpClient } from '@angular/common/http';
 import { JwtInterceptor } from '../shared/interceptors/jwt.interceptor';
+import { PRECONNECT_CHECK_BLOCKLIST } from '@angular/common';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideClientHydration(), provideHttpClient(), { provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true }
+  providers: [provideRouter(routes), provideClientHydration(), provideHttpClient(), { provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true }, { provide: PRECONNECT_CHECK_BLOCKLIST, useValue: 'https://avatars.githubusercontent.com' }
+
   ]
 };
