cedf5062157a101b71621716a47a7cc768a171ad
gajevski <mikolajxgajewski@gmail.com>
2024-01-29 12:01:49 +0100

    Fix fetch warning

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,14 +1,13 @@
 import { ApplicationConfig } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { provideRouter, withComponentInputBinding } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
-import { HTTP_INTERCEPTORS, provideHttpClient } from '@angular/common/http';
+import { HTTP_INTERCEPTORS, provideHttpClient, withFetch } from '@angular/common/http';
 import { JwtInterceptor } from '../shared/interceptors/jwt.interceptor';
 import { PRECONNECT_CHECK_BLOCKLIST } from '@angular/common';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideClientHydration(), provideHttpClient(), { provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true }, { provide: PRECONNECT_CHECK_BLOCKLIST, useValue: 'https://avatars.githubusercontent.com' }
-
+  providers: [provideRouter(routes, withComponentInputBinding()), provideClientHydration(), provideHttpClient(withFetch()), { provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true }, { provide: PRECONNECT_CHECK_BLOCKLIST, useValue: 'https://avatars.githubusercontent.com' }
   ]
 };
