fe8b83c3c44d977a934f13aa090b31235885cd8c
gajevski <mikolajxgajewski@gmail.com>
2024-01-29 13:16:27 +0100

    Refactor profile component to use signals

--- src/views/profile/profile.component.ts
+++ src/views/profile/profile.component.ts
@@ -1,9 +1,16 @@
-import { Component, inject } from '@angular/core';
+import { Component, Signal, inject } from '@angular/core';
 import { AuthService } from '../../shared/services/auth.service';
 import { UserService } from '../../shared/services/user.service';
-import { Observable } from 'rxjs';
 import { CommonModule, NgOptimizedImage } from '@angular/common';
 import { User } from '../../shared/models/user';
+import { toSignal } from '@angular/core/rxjs-interop';
+
+const initialValue: User = {
+  id: 0,
+  username: 'name',
+  image: '../../assets/images/squash-logo.png',
+  racket: 'racket',
+};
 
 @Component({
   selector: 'app-profile',
@@ -12,10 +19,11 @@ import { User } from '../../shared/models/user';
   templateUrl: './profile.component.html',
   styleUrl: './profile.component.scss'
 })
+
 export class ProfileComponent {
   private _authService: AuthService = inject(AuthService);
   private _userService: UserService = inject(UserService);
-  public user$: Observable<User> = this._userService.getUser();
+  public user: Signal<User> = toSignal(this._userService.getUser(), { initialValue });
 
   public logout(): void {
     this._authService.logout();
