567b04dd5fed6b5f3681d7812b1acc8da191c865
gajevski <mikolajxgajewski@gmail.com>
2024-01-30 21:22:22 +0100

    Use navigation component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,24 @@
-import { ChangeDetectionStrategy, Component, Inject, PLATFORM_ID } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Inject, PLATFORM_ID, inject } from '@angular/core';
 import { CommonModule, isPlatformBrowser } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { BottomNavComponent } from '../shared/components/bottom-nav/bottom-nav.component';
-import { BehaviorSubject } from 'rxjs';
+import { BehaviorSubject, Observable } from 'rxjs';
+import { NavbarComponent } from '../shared/components/navbar/navbar.component';
+import { User } from '../shared/models/user';
+import { UserService } from '../shared/services/user.service';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, BottomNavComponent],
+  imports: [CommonModule, RouterOutlet, BottomNavComponent, NavbarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class AppComponent {
+  private _userService: UserService = inject(UserService);
   static isBrowser: BehaviorSubject<boolean | null> = new BehaviorSubject<boolean | null>(null);
+  public user$: Observable<User> = this._userService.getUser();
 
   constructor(@Inject(PLATFORM_ID) private platformId: any) {
     AppComponent.isBrowser.next(isPlatformBrowser(platformId));
