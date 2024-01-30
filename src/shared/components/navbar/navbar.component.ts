bc5a498b7931d37e7ac89f413c56b3618be456bc
gajevski <mikolajxgajewski@gmail.com>
2024-01-30 21:23:05 +0100

    Remove navigation styles

--- src/shared/components/navbar/navbar.component.ts
+++ src/shared/components/navbar/navbar.component.ts
@@ -6,7 +6,6 @@ import { AuthService } from '../../services/auth.service';
   standalone: true,
   imports: [],
   templateUrl: './navbar.component.html',
-  styleUrl: './navbar.component.scss'
 })
 export class NavbarComponent {
   @Input({ required: true }) image!: string;
