b766820e1e084d069db5addca78eea11d1d0ea89
gajevski <mikolajxgajewski@gmail.com>
2024-02-02 13:17:22 +0100

    Add statistics into User model

--- src/shared/models/user.ts
+++ src/shared/models/user.ts
@@ -1,8 +1,10 @@
 import { Racket } from "./racket";
+import { Statistics } from "./statistics";
 
 export interface User {
     id: number;
     username: string;
     image: string;
     racket: Racket;
+    statistics: Statistics;
 }
