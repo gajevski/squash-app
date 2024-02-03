f77b54d2d2f74a390bd9fc930c5d1c45e3206ada
gajevski <mikolajxgajewski@gmail.com>
2024-02-03 13:01:09 +0100

    Add profile resolver

---
+++ src/views/profile/profile.resolver.ts
@@ -0,0 +1,10 @@
+import { ResolveFn } from '@angular/router';
+import { UserService } from '../../shared/services/user.service';
+import { inject } from '@angular/core';
+import { Observable } from 'rxjs';
+import { User } from '../../shared/models/user';
+
+export const profileResolver: ResolveFn<Observable<User>> = (route, state) => {
+  const userService: UserService = inject(UserService);
+  return userService.getUser();
+};
