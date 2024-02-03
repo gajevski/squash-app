59d064c03498a6b7e540d6effdfcb79be28d45c6
gajevski <mikolajxgajewski@gmail.com>
2024-02-03 13:08:19 +0100

    Add on push change detection

--- src/shared/components/activity-info/activity-info.component.ts
+++ src/shared/components/activity-info/activity-info.component.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 
 @Component({
   selector: 'app-activity-info',
   standalone: true,
   imports: [],
-  templateUrl: './activity-info.component.html'
+  templateUrl: './activity-info.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class ActivityInfoComponent {
 
