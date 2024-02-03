d9fd610ca5b0dfd6ef8423661adaa03a37c06d30
gajevski <mikolajxgajewski@gmail.com>
2024-02-03 13:08:25 +0100

    Add on push change detection

--- src/shared/components/racket-info/racket-info.component.ts
+++ src/shared/components/racket-info/racket-info.component.ts
@@ -1,11 +1,12 @@
-import { Component, Input } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
 import { Racket } from '../../models/racket';
 
 @Component({
   selector: 'app-racket-info',
   standalone: true,
   imports: [],
-  templateUrl: './racket-info.component.html'
+  templateUrl: './racket-info.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class RacketInfoComponent {
   @Input({ required: true }) racket!: Racket;
