aae57973aab95d43804da9fb39a5bec3b4eca5c9
gajevski <mikolajxgajewski@gmail.com>
2024-02-03 13:00:01 +0100

    Add activity component

---
+++ src/shared/components/activity-info/activity-info.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-activity-info',
+  standalone: true,
+  imports: [],
+  templateUrl: './activity-info.component.html',
+  styleUrl: './activity-info.component.scss'
+})
+export class ActivityInfoComponent {
+
+}
