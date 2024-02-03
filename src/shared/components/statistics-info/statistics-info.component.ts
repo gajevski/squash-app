b70c7b5f12c5fdfb8ae4c1006958325676d97e5b
gajevski <mikolajxgajewski@gmail.com>
2024-02-03 13:08:27 +0100

    Add on push change detection

--- src/shared/components/statistics-info/statistics-info.component.ts
+++ src/shared/components/statistics-info/statistics-info.component.ts
@@ -1,11 +1,12 @@
-import { Component, Input } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
 import { Statistics } from '../../models/statistics';
 
 @Component({
   selector: 'app-statistics-info',
   standalone: true,
   imports: [],
-  templateUrl: './statistics-info.component.html'
+  templateUrl: './statistics-info.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class StatisticsInfoComponent {
   @Input({ required: true }) public statistics!: Statistics;
