dd14ac003cd9bfd8c2621b49c5d070106e82a0d7
gajevski <mikolajxgajewski@gmail.com>
2024-02-05 20:03:38 +0100

    Update squash-tv components

---
+++ src/views/squash-tv/squash-tv.component.ts
@@ -0,0 +1,13 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-squash-tv',
+  standalone: true,
+  imports: [],
+  templateUrl: './squash-tv.component.html',
+  styleUrl: './squash-tv.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class SquashTvComponent {
+
+}
