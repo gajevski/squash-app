372eeed987c9ebffbf1ac454adeb73804debd67c
gajevski <mikolajxgajewski@gmail.com>
2024-02-06 22:17:35 +0100

    Remove onPush strategy

--- src/shared/components/bottom-nav/bottom-nav.component.ts
+++ src/shared/components/bottom-nav/bottom-nav.component.ts
@@ -7,13 +7,11 @@ import { Router, RouterLink } from '@angular/router';
   imports: [RouterLink],
   templateUrl: './bottom-nav.component.html',
   styleUrl: './bottom-nav.component.scss',
-  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class BottomNavComponent {
   public currentRoute!: string;
   
   constructor(private _router: Router) {}
-
 
   public isActive(route: string): boolean {
     return this._router.url === route;
