295f95afd408913ff1cb3949c193be453edd10f4
gajevski <mikolajxgajewski@gmail.com>
2024-02-07 13:37:14 +0100

    Use it in squash-tv component

--- src/views/squash-tv/squash-tv.component.ts
+++ src/views/squash-tv/squash-tv.component.ts
@@ -1,12 +1,13 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { HighlightCardComponent } from "../../shared/components/highlight-card/highlight-card.component";
 
 @Component({
-  selector: 'app-squash-tv',
-  standalone: true,
-  imports: [],
-  templateUrl: './squash-tv.component.html',
-  styleUrl: './squash-tv.component.scss',
-  changeDetection: ChangeDetectionStrategy.OnPush
+    selector: 'app-squash-tv',
+    standalone: true,
+    templateUrl: './squash-tv.component.html',
+    styleUrl: './squash-tv.component.scss',
+    changeDetection: ChangeDetectionStrategy.OnPush,
+    imports: [HighlightCardComponent]
 })
 export class SquashTvComponent {
 
