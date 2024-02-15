c871a4343a07b3451323a7658c9532727ac0b58d
gajevski <mikolajxgajewski@gmail.com>
2024-02-15 21:05:24 +0100

    Add register component

---
+++ src/views/register/register.component.ts
@@ -0,0 +1,17 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-register',
+  standalone: true,
+  imports: [RouterModule],
+  templateUrl: './register.component.html',
+  styleUrl: './register.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class RegisterComponent {
+
+  public onRegisterClick(): void {
+  // TODO: new auth logic
+  }
+}
