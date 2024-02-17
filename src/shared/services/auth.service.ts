fbdf764babecfa8a999c0ab1b4ba74aba3dad35e
gajevski <mikolajxgajewski@gmail.com>
2024-02-17 14:40:27 +0100

    Inject http service

--- src/shared/services/auth.service.ts
+++ src/shared/services/auth.service.ts
@@ -1,9 +1,13 @@
-import { Injectable } from '@angular/core';
+import { Injectable, inject } from '@angular/core';
 import { environment } from '../../enviroment';
+import { HttpClient } from '@angular/common/http';
+import { User } from '../models/user';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
+  private _http: HttpClient = inject(HttpClient);
   private _baseUrl: string = environment.apiBaseUrl;
+
 }
