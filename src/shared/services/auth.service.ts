ef51961de2b586923aaf97aa989d43bd0a04d39d
gajevski <mikolajxgajewski@gmail.com>
2024-02-17 15:01:59 +0100

    Add return type to login function

--- src/shared/services/auth.service.ts
+++ src/shared/services/auth.service.ts
@@ -2,6 +2,7 @@ import { Injectable, inject } from '@angular/core';
 import { environment } from '../../enviroment';
 import { HttpClient } from '@angular/common/http';
 import { User } from '../models/user';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -10,7 +11,7 @@ export class AuthService {
   private _http: HttpClient = inject(HttpClient);
   private _apiUrl: string = `${environment.apiBaseUrl}/auth/login`;
 
-  public login(user: User) {
+  public login(user: User): Observable<User> {
     return this._http.post<User>(this._apiUrl, user)
   }
 }
