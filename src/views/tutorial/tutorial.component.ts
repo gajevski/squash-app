45f6345595f2e8903626625553419d4ec9e312e4
gajevski <mikolajxgajewski@gmail.com>
2024-02-27 17:56:06 +0100

    Use onPush strategy

--- src/views/tutorial/tutorial.component.ts
+++ src/views/tutorial/tutorial.component.ts
@@ -1,11 +1,12 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 
 @Component({
   selector: 'app-tutorial',
   standalone: true,
   imports: [],
   templateUrl: './tutorial.component.html',
-  styleUrl: './tutorial.component.scss'
+  styleUrl: './tutorial.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class TutorialComponent {
 
