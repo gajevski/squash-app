173f0a9859f4338c431bb8cf4531708314af6dda
gajevski <mikolajxgajewski@gmail.com>
2024-02-28 19:00:38 +0100

    Add on push strategy

--- src/views/advanced-tutorial/advanced-tutorial.component.ts
+++ src/views/advanced-tutorial/advanced-tutorial.component.ts
@@ -1,11 +1,12 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 
 @Component({
   selector: 'app-advanced-tutorial',
   standalone: true,
   imports: [],
   templateUrl: './advanced-tutorial.component.html',
-  styleUrl: './advanced-tutorial.component.scss'
+  styleUrl: './advanced-tutorial.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class AdvancedTutorialComponent {
 
