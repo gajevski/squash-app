f3e34dbf5be68faeb82361cbaae79747c70c723f
gajevski <mikolajxgajewski@gmail.com>
2024-02-29 21:57:53 +0100

    Add tab change function

--- src/views/tutorial/tutorial.component.ts
+++ src/views/tutorial/tutorial.component.ts
@@ -1,5 +1,10 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 
+enum TutorialTab {
+  Serve = "serve",
+  Rally = "rally",
+}
+
 @Component({
   selector: 'app-tutorial',
   standalone: true,
@@ -9,5 +14,10 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class TutorialComponent {
+  public activeTutorialTab: string = 'serve';
+  public tutorialTab: typeof TutorialTab = TutorialTab;
 
+  public changeTab(tutorialTab: TutorialTab): void {
+    this.activeTutorialTab = tutorialTab;
+  }
 }
