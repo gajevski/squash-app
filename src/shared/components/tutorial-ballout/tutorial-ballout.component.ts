5b242536876073e670a498858d47a3ad76fc64c1
gajevski <mikolajxgajewski@gmail.com>
2024-03-01 22:06:12 +0100

    Add on push strategy

--- src/shared/components/tutorial-ballout/tutorial-ballout.component.ts
+++ src/shared/components/tutorial-ballout/tutorial-ballout.component.ts
@@ -1,11 +1,12 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 
 @Component({
   selector: 'app-tutorial-ballout',
   standalone: true,
   imports: [],
   templateUrl: './tutorial-ballout.component.html',
-  styleUrl: './tutorial-ballout.component.scss'
+  styleUrl: './tutorial-ballout.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class TutorialBalloutComponent {
 
