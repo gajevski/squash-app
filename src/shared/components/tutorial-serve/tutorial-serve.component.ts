1055cd38514eb211e1900d9b2fffe6156be2fe89
gajevski <mikolajxgajewski@gmail.com>
2024-03-01 22:03:04 +0100

    Add tutorial-serve component

---
+++ src/shared/components/tutorial-serve/tutorial-serve.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tutorial-serve',
+  standalone: true,
+  imports: [],
+  templateUrl: './tutorial-serve.component.html',
+  styleUrl: './tutorial-serve.component.scss'
+})
+export class TutorialServeComponent {
+
+}
