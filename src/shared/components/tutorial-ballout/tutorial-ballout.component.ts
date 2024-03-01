7e07e3cce39b118f1be573bd59bfc618a0054886
gajevski <mikolajxgajewski@gmail.com>
2024-03-01 22:05:48 +0100

    Add tutorial ballout component

---
+++ src/shared/components/tutorial-ballout/tutorial-ballout.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tutorial-ballout',
+  standalone: true,
+  imports: [],
+  templateUrl: './tutorial-ballout.component.html',
+  styleUrl: './tutorial-ballout.component.scss'
+})
+export class TutorialBalloutComponent {
+
+}
