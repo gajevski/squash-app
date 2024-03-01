5c901296cdcb935e9f7d1b9d877b5fa6043f8746
gajevski <mikolajxgajewski@gmail.com>
2024-03-01 22:05:01 +0100

    Add on push strategy

--- src/shared/components/tutorial-rally/tutorial-rally.component.ts
+++ src/shared/components/tutorial-rally/tutorial-rally.component.ts
@@ -1,11 +1,12 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 
 @Component({
   selector: 'app-tutorial-rally',
   standalone: true,
   imports: [],
   templateUrl: './tutorial-rally.component.html',
-  styleUrl: './tutorial-rally.component.scss'
+  styleUrl: './tutorial-rally.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class TutorialRallyComponent {
 
