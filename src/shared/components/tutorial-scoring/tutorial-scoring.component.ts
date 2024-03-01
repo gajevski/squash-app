eadcd2ecf27b5e395d135acb2d984acab7fff787
gajevski <mikolajxgajewski@gmail.com>
2024-03-01 22:08:29 +0100

    Add on push strategy

--- src/shared/components/tutorial-scoring/tutorial-scoring.component.ts
+++ src/shared/components/tutorial-scoring/tutorial-scoring.component.ts
@@ -1,11 +1,12 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 
 @Component({
   selector: 'app-tutorial-scoring',
   standalone: true,
   imports: [],
   templateUrl: './tutorial-scoring.component.html',
-  styleUrl: './tutorial-scoring.component.scss'
+  styleUrl: './tutorial-scoring.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class TutorialScoringComponent {
 
