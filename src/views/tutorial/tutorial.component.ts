6e79baa3ed8bf78991eafb409ad1f2808b253294
gajevski <mikolajxgajewski@gmail.com>
2024-03-01 22:07:34 +0100

    Use tutorial ballout component

--- src/views/tutorial/tutorial.component.ts
+++ src/views/tutorial/tutorial.component.ts
@@ -1,6 +1,7 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { TutorialServeComponent } from "../../shared/components/tutorial-serve/tutorial-serve.component";
 import { TutorialRallyComponent } from "../../shared/components/tutorial-rally/tutorial-rally.component";
+import { TutorialBalloutComponent } from "../../shared/components/tutorial-ballout/tutorial-ballout.component";
 
 enum TutorialTab {
   Serve = "serve",
@@ -15,7 +16,7 @@ enum TutorialTab {
     templateUrl: './tutorial.component.html',
     styleUrl: './tutorial.component.scss',
     changeDetection: ChangeDetectionStrategy.OnPush,
-    imports: [TutorialServeComponent, TutorialRallyComponent]
+    imports: [TutorialServeComponent, TutorialRallyComponent, TutorialBalloutComponent]
 })
 export class TutorialComponent {
   public tutorialTab: typeof TutorialTab = TutorialTab;
