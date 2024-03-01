bc6886921bd1a5c9e50853124237ffac5e787cdf
gajevski <mikolajxgajewski@gmail.com>
2024-03-01 22:04:38 +0100

    Add tutorial-rally component

---
+++ src/shared/components/tutorial-rally/tutorial-rally.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tutorial-rally',
+  standalone: true,
+  imports: [],
+  templateUrl: './tutorial-rally.component.html',
+  styleUrl: './tutorial-rally.component.scss'
+})
+export class TutorialRallyComponent {
+
+}
