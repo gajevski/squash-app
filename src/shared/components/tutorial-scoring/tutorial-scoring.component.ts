6f0ca626cd8c09d996e037569597e12719450244
gajevski <mikolajxgajewski@gmail.com>
2024-03-01 22:07:57 +0100

    Add tutorial-scoring component

---
+++ src/shared/components/tutorial-scoring/tutorial-scoring.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tutorial-scoring',
+  standalone: true,
+  imports: [],
+  templateUrl: './tutorial-scoring.component.html',
+  styleUrl: './tutorial-scoring.component.scss'
+})
+export class TutorialScoringComponent {
+
+}
