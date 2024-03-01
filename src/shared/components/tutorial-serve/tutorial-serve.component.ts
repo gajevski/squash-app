3f35a3fefef5626188835a0d1c5d7ebdd2f1da41
gajevski <mikolajxgajewski@gmail.com>
2024-03-01 22:03:29 +0100

    Add on push strategy

--- src/shared/components/tutorial-serve/tutorial-serve.component.ts
+++ src/shared/components/tutorial-serve/tutorial-serve.component.ts
@@ -1,11 +1,12 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 
 @Component({
   selector: 'app-tutorial-serve',
   standalone: true,
   imports: [],
   templateUrl: './tutorial-serve.component.html',
-  styleUrl: './tutorial-serve.component.scss'
+  styleUrl: './tutorial-serve.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class TutorialServeComponent {
 
