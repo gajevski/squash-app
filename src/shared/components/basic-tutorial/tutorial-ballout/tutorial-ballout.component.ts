9d9db1d767eb165736feb6f17d8bb8f14bba2495
gajevski <mikolajxgajewski@gmail.com>
2024-03-02 18:41:59 +0100

    Refactor basic tutorial components

---
+++ src/shared/components/basic-tutorial/tutorial-ballout/tutorial-ballout.component.ts
@@ -0,0 +1,13 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tutorial-ballout',
+  standalone: true,
+  imports: [],
+  templateUrl: './tutorial-ballout.component.html',
+  styleUrl: './tutorial-ballout.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class TutorialBalloutComponent {
+
+}
