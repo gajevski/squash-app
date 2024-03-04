448d55115b840f80aeaef08511b155f452f7e6a5
gajevski <mikolajxgajewski@gmail.com>
2024-03-04 20:43:56 +0100

    Add get function

--- src/views/tutorial/tutorial.service.ts
+++ src/views/tutorial/tutorial.service.ts
@@ -1,9 +1,15 @@
-import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Injectable, inject } from '@angular/core';
+import { environment } from '../../enviroment';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TutorialService {
+  private _http: HttpClient = inject(HttpClient);
 
-  constructor() { }
+  public getBasicTutorialProgress(): Observable<any> {
+    return this._http.get<any>(`${environment.apiBaseUrl}/basic-tutorial/progress`);
+  }
 }
