737f098a238ae9d507154b84a32aab0af3d908e4
gajevski <mikolajxgajewski@gmail.com>
2024-03-06 20:21:04 +0100

    Add mock data

--- src/views/tutorial/tutorial.component.ts
+++ src/views/tutorial/tutorial.component.ts
@@ -5,6 +5,8 @@ import { TutorialBalloutComponent } from "../../shared/components/basic-tutorial
 import { TutorialScoringComponent } from "../../shared/components/basic-tutorial/tutorial-scoring/tutorial-scoring.component";
 import { ActivatedRoute } from '@angular/router';
 import { BasicTutorial } from '../../shared/models/basic-tutorial';
+import { TutorialService } from './tutorial.service';
+import { Observable, take } from 'rxjs';
 
 enum TutorialTab {
   Serve = "serve",
@@ -14,20 +16,35 @@ enum TutorialTab {
 }
 
 @Component({
-    selector: 'app-tutorial',
-    standalone: true,
-    templateUrl: './tutorial.component.html',
-    styleUrl: './tutorial.component.scss',
-    changeDetection: ChangeDetectionStrategy.OnPush,
-    imports: [TutorialServeComponent, TutorialRallyComponent, TutorialBalloutComponent, TutorialScoringComponent]
+  selector: 'app-tutorial',
+  standalone: true,
+  templateUrl: './tutorial.component.html',
+  styleUrl: './tutorial.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  imports: [TutorialServeComponent, TutorialRallyComponent, TutorialBalloutComponent, TutorialScoringComponent]
 })
 export class TutorialComponent {
   public tutorialTab: typeof TutorialTab = TutorialTab;
   public activeTutorialTab: string = this.tutorialTab.Serve;
   private _route: ActivatedRoute = inject(ActivatedRoute);
+  private _tutorialService: TutorialService = inject(TutorialService);
   public progress: BasicTutorial = this._route.snapshot.data['progress'];
 
   public changeTutorialTab(tutorialTab: TutorialTab): void {
     this.activeTutorialTab = tutorialTab;
+  }
+
+  public updateProgress(): void {
+    this._tutorialService.updateBasicTutorialProgress({
+      userId: 1,
+      isServeFinished: true,
+      isRallyFinished: false,
+      isBalloutFinished: false,
+      isScoringFinished: false
+    })
+      .pipe(
+        take(1)
+      )
+      .subscribe()
   }
 }
