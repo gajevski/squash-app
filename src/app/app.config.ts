e8fb2733f02a03f40e45e3d827f4e2400aaac415
gajevski <mikolajxgajewski@gmail.com>
2024-03-10 18:44:05 +0100

    Use logout interceptor

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -5,8 +5,9 @@ import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
 import { provideHttpClient, withFetch, withInterceptors } from '@angular/common/http';
 import { jwtInterceptor } from '../shared/interceptors/jwt.interceptor';
+import { logoutInterceptor } from '../shared/interceptors/logout.interceptor';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes, withViewTransitions(), withComponentInputBinding()), provideClientHydration(), provideHttpClient(withInterceptors([jwtInterceptor]), withFetch())
+  providers: [provideRouter(routes, withViewTransitions(), withComponentInputBinding()), provideClientHydration(), provideHttpClient(withInterceptors([jwtInterceptor, logoutInterceptor]), withFetch())
   ]
 };
