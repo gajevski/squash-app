cd3384c14d009c1f0b5bce53242701f2e2650e24
gajevski <mikolajxgajewski@gmail.com>
2024-03-10 18:17:56 +0100

    Add update progress method

--- src/views/advanced-tutorial/advanced-tutorial.component.ts
+++ src/views/advanced-tutorial/advanced-tutorial.component.ts
@@ -1,6 +1,8 @@
 import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { AdvancedTutorial } from '../../shared/models/advanced-tutorial';
+import { AdvancedTutorialService } from './advanced-tutorial.service';
+import { take, tap } from 'rxjs';
 
 enum TutorialTab {
   One = "1",
@@ -22,6 +24,7 @@ export class AdvancedTutorialComponent {
   public activeTutorialTab: string = this.tutorialTab.One;
   private _route: ActivatedRoute = inject(ActivatedRoute);
   private _router: Router = inject(Router);
+  private _tutorialService: AdvancedTutorialService = inject(AdvancedTutorialService);
   public progress: AdvancedTutorial = this._route.snapshot.data['progress'];
 
   public changeTutorialTab(tutorialTab: TutorialTab): void {
@@ -33,17 +36,28 @@ export class AdvancedTutorialComponent {
 
     switch (this.activeTutorialTab) {
       case TutorialTab.One:
+        advancedTutorial.one = true;
         this.changeTutorialTab(TutorialTab.Two);
         break;
       case TutorialTab.Two:
+        advancedTutorial.two = true;
         this.changeTutorialTab(TutorialTab.Three);
         break;
       case TutorialTab.Three:
+        advancedTutorial.three = true;
         this.changeTutorialTab(TutorialTab.Four);
         break;
       case this.tutorialTab.Four:
+        advancedTutorial.four = true;
         this._router.navigate(['/home'])
         break;
     }
+
+    this._tutorialService.updateAdvancedTutorialProgress(advancedTutorial)
+      .pipe(
+        take(1),
+        tap((advancedTutorial: AdvancedTutorial) => this.progress = advancedTutorial)
+      )
+      .subscribe()
   }
 }
