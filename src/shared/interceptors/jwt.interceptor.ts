369a6b2d7bc21837776805c8913bec3894d2097d
gajevski <mikolajxgajewski@gmail.com>
2024-03-10 18:38:59 +0100

    Simple fixes in jwt interceptor

--- src/shared/interceptors/jwt.interceptor.ts
+++ src/shared/interceptors/jwt.interceptor.ts
@@ -1,10 +1,9 @@
 import { HttpInterceptorFn } from '@angular/common/http';
 import { inject } from '@angular/core';
 import { LocalstorageService } from '../services/localstorage.service';
-import { AccessToken } from '../models/access-token';
 
 export const jwtInterceptor: HttpInterceptorFn = (req, next) => {
-  const localstorageService = inject(LocalstorageService);
+  const localstorageService: LocalstorageService = inject(LocalstorageService);
   const accessToken: string | null = localstorageService.getItem('access_token');
   const trimmedToken = accessToken?.startsWith('"') && accessToken.endsWith('"') ? accessToken.slice(1, -1) : accessToken;
 
