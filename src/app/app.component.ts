3e83c7db09b2d933ddebfc7893f4dc061b4aebe1
gajevski <mikolajxgajewski@gmail.com>
2024-03-13 20:05:37 +0100

    Refactor action button

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,18 +6,25 @@ import { BehaviorSubject } from 'rxjs';
 import { NavbarComponent } from '../shared/components/navbar/navbar.component';
 import { AuthService } from '../shared/services/auth.service';
 
+
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [CommonModule, RouterOutlet, BottomNavComponent, NavbarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
+
 export class AppComponent {
   static isBrowser: BehaviorSubject<boolean | null> = new BehaviorSubject<boolean | null>(null);
   public isUserLoggedIn: BehaviorSubject<boolean> = inject(AuthService).isLoggedIn$;
   public showButtons: boolean = false;
+  public buttons = [
+    { label: 'Akcja 1', color: 'bg-blue-500 hover:bg-blue-700', action: () => {/* akcja */} },
+    { label: 'Akcja 2', color: 'bg-green-500 hover:bg-green-700', action: () => {/* akcja */} },
+    { label: 'Akcja 3', color: 'bg-red-500 hover:bg-red-700', action: () => {/* akcja */} },
+  ];
 
   constructor(@Inject(PLATFORM_ID) private platformId: any) {
     AppComponent.isBrowser.next(isPlatformBrowser(platformId));
