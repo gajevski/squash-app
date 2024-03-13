56f795053e5329071ec91a011222c970cf3b5176
gajevski <mikolajxgajewski@gmail.com>
2024-03-13 20:20:02 +0100

    Add animations

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -6,8 +6,9 @@ import { provideClientHydration } from '@angular/platform-browser';
 import { provideHttpClient, withFetch, withInterceptors } from '@angular/common/http';
 import { jwtInterceptor } from '../shared/interceptors/jwt.interceptor';
 import { logoutInterceptor } from '../shared/interceptors/logout.interceptor';
+import { provideAnimations } from '@angular/platform-browser/animations';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes, withViewTransitions(), withComponentInputBinding()), provideClientHydration(), provideHttpClient(withInterceptors([jwtInterceptor, logoutInterceptor]), withFetch())
+  providers: [provideRouter(routes, withViewTransitions(), withComponentInputBinding()), provideClientHydration(), provideAnimations(), provideHttpClient(withInterceptors([jwtInterceptor, logoutInterceptor]), withFetch())
   ]
 };
