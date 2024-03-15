eaa5b0f55fafa229947e977ae52cd53869be72e3
gajevski <mikolajxgajewski@gmail.com>
2024-03-15 22:14:22 +0100

    Create main action button component

---
+++ src/shared/components/main-action-button/main-action-button.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-main-action-button',
+  standalone: true,
+  imports: [],
+  templateUrl: './main-action-button.component.html',
+  styleUrl: './main-action-button.component.scss'
+})
+export class MainActionButtonComponent {
+
+}
