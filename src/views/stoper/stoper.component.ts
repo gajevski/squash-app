0b9da30e23928c7d167d99b95234ef527e80d627
gajevski <mikolajxgajewski@gmail.com>
2024-03-28 20:20:34 +0100

    Add stoper component

---
+++ src/views/stoper/stoper.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-stoper',
+  standalone: true,
+  imports: [],
+  templateUrl: './stoper.component.html',
+  styleUrl: './stoper.component.scss'
+})
+export class StoperComponent {
+
+}
