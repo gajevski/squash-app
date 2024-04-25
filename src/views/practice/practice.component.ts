c266b291cc57688ed5e5c6422811104c60cac2fd
gajevski <mikolajxgajewski@gmail.com>
2024-04-25 21:47:01 +0200

    Remove scss files

--- src/views/practice/practice.component.ts
+++ src/views/practice/practice.component.ts
@@ -5,7 +5,6 @@ import { RouterModule } from '@angular/router';
     selector: 'app-practice',
     standalone: true,
     templateUrl: './practice.component.html',
-    styleUrl: './practice.component.scss',
     changeDetection: ChangeDetectionStrategy.OnPush,
     imports: [RouterModule]
 })
